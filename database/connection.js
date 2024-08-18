1f6435b3caac6c18056e08a5d2bb654190c3bf62
santipgz <santiagopalomo22@gmail.com>
2024-08-18 11:21:45 +0200

    prueba

--- database/connection.js
+++ database/connection.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const connection = async () => {
     try {
+        mongoose.set("strictQuery", false);
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
