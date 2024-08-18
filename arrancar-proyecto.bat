fa8a76cbf4e8254cb0051cd5ac2eaa182fa93621
santipgz <santiagopalomo22@gmail.com>
2024-08-18 12:05:40 +0200

    prueba

--- arrancar-proyecto.bat
+++ arrancar-proyecto.bat
@@ -1,2 +1,2 @@
-start cmd.exe /k "cd C:\Program Files\MongoDB\Server\5.0\bin && mongod.exe --dbpath C:/data5/db"
-start cmd.exe /k "cd C:\xampp\htdocs\TFM_SantiagoPalomoGutierrez\API_Rest_agmtweet && npm start"
+
+start cmd.exe /k "cd C:\Users\aidat\Desktop\API_Rest_agmtweet && npm start"
